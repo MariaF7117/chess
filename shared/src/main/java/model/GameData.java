4f0b4135eed6d2cccb035a1b99cbf631b30f4db4
MariaF7117 <mgf11@byu.edu>
2025-02-28 14:08:20 -0700

    met with TA and implemented models

--- shared/src/main/java/model/GameData.java
+++ shared/src/main/java/model/GameData.java
@@ -1,4 +1,63 @@
 package model;
 
+import chess.ChessGame;
+
+import java.util.Objects;
+
 public class GameData {
+
+    private final int gameId;
+    private final String whiteUsername;
+    private final String blackUsername;
+    private final String gameName;
+    private final ChessGame game;
+
+    GameData(int gameId, String whiteUsername, String blackUsername, String gameName) {
+        this.gameId = gameId;
+        this.whiteUsername = whiteUsername;
+        this.blackUsername = blackUsername;
+        this.gameName = gameName;
+        this.game = new ChessGame();
+    }
+
+    public int getGameId() {
+        return gameId;
+    }
+    public String getWhiteUsername() {
+        return whiteUsername;
+    }
+    public String getBlackUsername() {
+        return blackUsername;
+    }
+    public String getGameName() {
+        return gameName;
+    }
+    public ChessGame getGame() {
+        return game;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        GameData gameData = (GameData) o;
+        return gameId == gameData.gameId && Objects.equals(whiteUsername, gameData.whiteUsername) && Objects.equals(blackUsername, gameData.blackUsername) && Objects.equals(gameName, gameData.gameName) && Objects.equals(game, gameData.game);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(gameId, whiteUsername, blackUsername, gameName, game);
+    }
+
+    @Override
+    public String toString() {
+        return "GameData{" +
+                "gameId=" + gameId +
+                ", whiteUsername='" + whiteUsername + '\'' +
+                ", blackUsername='" + blackUsername + '\'' +
+                ", gameName='" + gameName + '\'' +
+                ", game=" + game +
+                '}';
+    }
 }
