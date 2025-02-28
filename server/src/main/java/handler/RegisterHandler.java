51a83b34fd8cdcba854022178aa8cae22f125662
MariaF7117 <mgf11@byu.edu>
2025-02-28 13:23:41 -0700

    working on registerHandler... have to implement register in UserService

--- server/src/main/java/handler/RegisterHandler.java
+++ server/src/main/java/handler/RegisterHandler.java
@@ -1,11 +1,30 @@
 package handler;
 
-public class RegisterHandler {
-    //JSON string version of an object
-    //converting JSON string into an object for the Service
-    //create method handleRequest();
-        //for LoginHandler do loginRequest
-    public Object registerNewUser(username){
+import com.google.gson.Gson;
+import service.AuthService;
+import spark.Request;
+import spark.Response;
+import service.UserService;
+import model.AuthData;
 
+public class RegisterHandler {
+    private final Gson serializer = new Gson();
+
+    public Object registerNewUser(Request req, Response res, UserService userService, AuthService authService) {
+        res.type("application/json");
+        try {
+            UserRequest userRequest = serializer.fromJson(req.body(), UserRequest.class);
+            AuthData authData = userService.register(userRequest.username, userRequest.password, userRequest.email);
+            res.status(200);
+            return serializer.toJson(authData);
+        } catch (Exception e) {
+            return new ErrorHandler().handleError(e, res, 400);
+        }
+    }
+
+    private static class UserRequest {
+        String username;
+        String password;
+        String email;
     }
 }
