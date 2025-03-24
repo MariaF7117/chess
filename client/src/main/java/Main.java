7c2ed2cb72abbe4ca06caf13e4f8014f648a02ea
MariaF7117 <mgf11@byu.edu>
2025-03-24 10:34:05 -0600

    trying to figure out the functionality of Menu and Main and fill them out before implementing ServerFacade

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,31 @@
 import chess.*;
+import ui.Menu;
+
+import java.util.Scanner;
+
+import static java.awt.SystemColor.menu;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        String serverUrl = "http://localhost:8080";
+        Menu menu = new Menu(serverUrl);
+
+        System.out.println("♕ CS240 Chess Client");
+        System.out.println("Type 'Help' to get started");
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                menu.printInput();
+                String input = scanner.nextLine().trim();
+
+                if (input.equalsIgnoreCase("quit")) {
+                    break;
+                }
+
+                if (!input.isEmpty()) {
+                    menu.handleInput(input);
+                }
+            }
+        }
     }
 }
---
+++ client/src/main/java/ui/Menu.java
@@ -0,0 +1,15 @@
+package ui;
+public class Menu {
+
+
+    public Menu(String portNumber){
+    }
+
+    public void handleInput(String input){
+
+    }
+
+   public String printInput(){
+        return "";
+   }
+}
