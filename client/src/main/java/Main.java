fefc91fe4c5f5737512de80dcb37648d2b6e6a9e
MariaF7117 <mgf11@byu.edu>
2025-03-24 11:04:30 -0600

    now filling out outline of Menu and Main

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,9 +1,6 @@
-import chess.*;
 import ui.Menu;
 
 import java.util.Scanner;
-
-import static java.awt.SystemColor.menu;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,7 +12,6 @@ public class Main {
 
         try (Scanner scanner = new Scanner(System.in)) {
             while (true) {
-                menu.printInput();
                 String input = scanner.nextLine().trim();
 
                 if (input.equalsIgnoreCase("quit")) {
--- client/src/main/java/ui/Menu.java
+++ client/src/main/java/ui/Menu.java
@@ -1,15 +1,88 @@
 package ui;
+
+import model.UserData;
+
 public class Menu {
 
+    private final ServerFacade serverFacade;
+    private boolean isLoggedIn;
+    private String authToken;
 
-    public Menu(String portNumber){
+    private String username;
+
+    public Menu(String serverUrl){
+        this.serverFacade = new ServerFacade(serverUrl);
+        this.isLoggedIn = false;
+        this.authToken = null;
     }
 
     public void handleInput(String input){
+        String[] tokens = input.split("\\s+", 2);
+        input = tokens[0].toLowerCase();
 
+        if(!isLoggedIn){
+            handlePreLoggedIn(input, tokens);
+        }
+        else if(isLoggedIn){
+            handlePostLoggedIn(input,tokens);
+        }
     }
 
-   public String printInput(){
+    public void handlePreLoggedIn(String input, String[] tokens) {
+        switch (input) {
+            case "help":
+                printPreloginHelp();
+                break;
+            case "login":
+                login(tokens);
+                break;
+            case "register":
+                register(tokens);
+                break;
+        }
+    }
+    public void handlePostLoggedIn(String input, String[] tokens){
+        switch (input) {
+            case "help":
+                printPostloginHelp();
+                break;
+            case "logout":
+                logout();
+                break;
+            case "create":
+                createGame(tokens);
+                break;
+            case "list":
+                listGames();
+                break;
+            case "play":
+                joinGame(tokens);
+                break;
+        }
+    }
+
+    public String printPreloginHelp(){
         return "";
-   }
+    }
+    public String printPostloginHelp(){
+        return "";
+    }
+    public void login(String[] tokens){
+
+    }
+    public void register(String[] tokens){
+
+    }
+    public void logout(){
+
+    }
+    public void createGame(String[] tokens){
+
+    }
+    public void listGames(){
+
+    }
+    public void joinGame(String[] tokens){
+
+    }
 }
